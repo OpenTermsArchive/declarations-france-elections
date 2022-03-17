e6b53e4d13f1d4f48b51f53a9d5a97acef59913c
Clément Biron <clement.biron@gmail.com>
2022-03-17 14:27:53 +0100

    Remove locale2 links query parameter, close #80

--- declarations/Facebook.filters.js
+++ declarations/Facebook.filters.js
@@ -3,12 +3,24 @@ export function removeEmptyAnchorsLinks(document) {
     link.removeAttribute("href")
   );
 }
+
 export function removeTrackingIDs(document) {
   document.querySelectorAll("a").forEach((el) => {
     const href = el.getAttribute("href");
     const params = new URLSearchParams(href);
     if (params.has("h")) {
       params.delete("h");
+      el.setAttribute("href", params.toString());
+    }
+  });
+}
+
+export function removeLocaleFromUrls(document) {
+  document.querySelectorAll("a").forEach((el) => {
+    const href = el.getAttribute("href");
+    const params = new URLSearchParams(href);
+    if (params.has("locale2")) {
+      params.delete("locale2");
       el.setAttribute("href", params.toString());
     }
   });
