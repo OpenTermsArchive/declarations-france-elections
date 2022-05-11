3cc8895356fee14824761f5be718940242eb8ebc
Martin Ratinaud <martinratinaud@gmail.com>
2022-05-11 18:04:01 +0400

    Lint all files

--- declarations/Instagram.filters.js
+++ declarations/Instagram.filters.js
@@ -1,18 +1,19 @@
 export function removeTrackingIDs(document) {
-  removeQueryParam(document, "h");
+  removeQueryParam(document, 'h');
 }
 
 export function removeEQueryParam(document) {
-  removeQueryParam(document, "e");
+  removeQueryParam(document, 'e');
 }
 
 function removeQueryParam(document, queryParam) {
-  document.querySelectorAll("a").forEach((el) => {
-    const href = el.getAttribute("href");
+  document.querySelectorAll('a').forEach(el => {
+    const href = el.getAttribute('href');
     const params = new URLSearchParams(href);
+
     if (params.has(queryParam)) {
       params.delete(queryParam);
-      el.setAttribute("href", params.toString());
+      el.setAttribute('href', params.toString());
     }
   });
 }
